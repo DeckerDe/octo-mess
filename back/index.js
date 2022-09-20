1dd2b7350937561fc9335d3564932e8b7159f7ad
Deiwid Decker <deiwidecker@gmail.com>
2022-09-20 11:24:51 -0300

    okta redirect flow working

--- back/index.js
+++ back/index.js
@@ -1,48 +1,58 @@
 const express = require('express')
-const OktaJwtVerifier = require('@okta/jwt-verifier')
-
-const oktaJwtVerifier = new OktaJwtVerifier({
-    issuer: 'https://dev-97369796.okta.com/oauth2/default'
-})
-
-const audience = 'api://default'
+const session = require('express-session')
+const passport = require('passport')
+const { Strategy } = require('passport-openidconnect')
 
 app = express()
 
-const authRequired = async (req, res,next)=>{
-    const authHeader = req.headers.authorization || ''
-    const match = authHeader.match(/Bearer (.+)/)
+app.use(session({
+    secret: 'myNewSecret',
+    resave: false,
+    saveUninitialized: true
+}))
 
-    if(!match){
-        return res.status(401).send()
-    }
+app.use(passport.initialize());
+app.use(passport.session());
 
-    try{
-        const accessToken = match[1]
-        if(!accessToken){
-            return res.status(401, 'Not authorized').send()
-        }
-        req.jwt = await oktaJwtVerifier.verifyAccessToken(accessToken, audience)
-        next()
-    } catch(err){
-        return res.status(401).send(err.message)
-    }
-}
+passport.use('oidc', new Strategy({
+    issuer: 'https://dev-97369796.okta.com/oauth2/default',
+    authorizationURL: 'https://dev-97369796.okta.com/oauth2/default/v1/authorize',
+    tokenURL: 'https://dev-97369796.okta.com/oauth2/default/v1/token',
+    userInfoURL: 'https://dev-97369796.okta.com/oauth2/default/v1/userinfo',
+    clientID: '0oa6kwa0h4Fy66SWi5d7',
+    clientSecret: '9MtTRX_Cbjp6P5Fd_BduoAzwkuFx3PNn4aqXp2LJ',
+    callbackURL: 'http://localhost:3000/authorization-code/callback',
+    scope: 'openid profile'
+}, (issuer, profile, done)=>{
+    return done (null, profile)
+}))
 
-app.get('/', (req, res)=>{
-    res.send('hello-world')
+passport.serializeUser((user, next)=>{
+    next(null, user)
 })
 
-
-app.get('/api/hello', (req,res)=>{
-    res.send('Hello world')
-})
-
-app.get('/api/whoami', authRequired, (req,res)=>{
-    res.json(req.jwt?.claims)
+passport.deserializeUser((obj, next)=>{
+    next(null, obj)
 })
 
 const port = process.env.PORT || 3000  //TODO: Voltar para 80 qdo for subir para o fargate.
 app.listen(port, () =>
     console.log(`Server is listening on port ${port}.`)
 )
+
+app.use('/authorization-code/callback', passport.authenticate('oidc', {failureRedirect: '/error'}),
+    (req, res) => {
+    res.redirect('/')
+    })
+
+app.post('/logout', (req, res)=>{
+    req.logout()
+    req.session.destroy()
+    res.redirect('/')
+})
+
+app.get('/login', passport.authenticate('oidc'))
+
+app.get('/', (req, res)=>{
+    res.send('hello-world, we know the user ?' + req.isAuthenticated())
+})
